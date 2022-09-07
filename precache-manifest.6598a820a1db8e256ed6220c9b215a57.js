cabc5a8a7c608bcf0357110fcc16b7bbf3c83b80
ahmad <ahmadaug20@gmail.com>
2022-09-07 16:53:09 +0530

    hello

---
+++ precache-manifest.6598a820a1db8e256ed6220c9b215a57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec816c71ef268e17c98b",
+    "url": "/static/css/main.503f7013.chunk.css"
+  },
+  {
+    "revision": "ec816c71ef268e17c98b",
+    "url": "/static/js/main.ec816c71.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1012b21fa86e024c3ad885749f07eb7f",
+    "url": "/index.html"
+  }
+];
